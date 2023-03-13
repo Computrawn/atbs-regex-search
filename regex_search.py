f8e45bfa84ca919d82abb074c4c6e87b76957d60
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-03-13 12:37:33 -0500

    TODO list outlined.

--- regex_search.py
+++ regex_search.py
@@ -5,3 +5,11 @@ For more info, see project_details.txt.
 """
 
 import re
+
+
+def regex_search():
+    # TODO: Open all .txt files in a folder.
+
+    # TODO: Search files for user-defined regular expression.
+
+    # TODO: Print results to screen.
