3110cfddbefa20bad363582fd39927a00e5d443c
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-03-13 16:29:47 -0500

    Added some dummy files to text_files directory and finished first pass at regex_search function.

--- regex_search.py
+++ regex_search.py
@@ -4,12 +4,29 @@ regex_search.py — An exercise in reading and writing files.
 For more info, see project_details.txt.
 """
 
+from pathlib import Path
 import re
 
 
 def regex_search():
-    # TODO: Open all .txt files in a folder.
+    """Open and read all .txt files in text_files directory."""
+    file_path = Path.cwd() / Path("text_files")
+    user_input = input("Please type a regular expression to start search: ")
+    user_regex = re.compile(user_input)
+    found = 0
 
-    # TODO: Search files for user-defined regular expression.
+    for text_file in file_path.glob("*.txt"):
+        contents = text_file.read_text()
 
-    # TODO: Print results to screen.
+        # Search contents for user-defined regular expression.
+        mo = user_regex.findall(contents)
+
+        # Print results to screen.
+        for i in range(len(mo)):
+            print(mo[i])
+            found += 1
+
+    print(f"Your search yielded {found} matches.")
+
+
+regex_search()
